f2da1456de7fbc7b686c860a047512a52effd41c
andresgfranco <andresgfranco0@gmail.com>
2021-08-12 22:22:51 -0500

    Adding readme file and code solution for the algorithm by looking the prime numbers of n

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+Method that calculates the fewest
+number of operations needed to result in exactly
+n H characters in the file
+'''
+
+def minOperations(n):
+    primeNumber = 2
+    minOperations = 0
+
+    if n <= 1:
+        return 0
+    else:
+        while n != 1:
+          if n % primeNumber == 0:
+            n = n / primeNumber
+            minOperations = minOperations + primeNumber
+          else:
+            primeNumber = primeNumber + 1
+
+    return minOperations
